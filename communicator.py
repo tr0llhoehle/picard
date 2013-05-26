2e393931e36a51ebeb74bff9f54704c13c6efdf1
Kai Hermann <kai.uwe.hermann@gmail.com>
2013-05-26 21:40:09 +0200

    Implemented digest access authentication to enable authenticated control messages
    Signed-off-by: Kai Hermann <kai.uwe.hermann@gmail.com>

--- communicator.py
+++ communicator.py
@@ -3,26 +3,80 @@ import curses
 import time
 import signal
 import sys
+import md5
+import json
+import argparse
+
+username = ''
+password = ''
+ip = '127.0.0.1'
+port = 5005
+sourceport = 6666
 
 class Communication:
-	def __init__(self):
-		self.UDP_IP = "127.0.0.1"
-		self.UDP_PORT = 5005
+	def __init__(self, screen, username, password, ip, port, sourceport):
+		self.screen = screen
+		self.UDP_IP =  ip
+		self.UDP_PORT = port
 		self.sequence = 0
-		self.sock = socket.socket(socket.AF_INET, # Internet
-	                     socket.SOCK_DGRAM) # UDP
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.sock.bind(('127.0.0.1', sourceport))
+		self.realm = ''
+		self.nonce = ''
+		self.username = username
+		self.password = password
 
 	def sendmessage(self, command):
-		message = '{"sequence":'+str(self.sequence)+',"command":"'+command+'"}'
 		self.sequence += 1
+		hash = self.gethash()
+		message = '{"sequence":'+str(self.sequence)+',"command":"'+command+'", "username":"'+self.username+'", "hash":"'+hash+'"}'
 		self.sock.sendto(message, (self.UDP_IP, self.UDP_PORT))
 
+	def auth(self):
+		message = '{"sequence":0, "command":"requestauth", "username":"'+self.username+'"}'
+		self.sock.sendto(message, (self.UDP_IP, self.UDP_PORT))
+		data, addr = self.sock.recvfrom(1024)
+		decoded = json.loads(data)
+
+		self.screen.clear()
+		self.screen.refresh()
+		self.screen.addstr(0,0,decoded.get('error', 'success'))
+		if(decoded.get('error', 'success') == 'success'):
+			self.realm = decoded['realm']
+			self.nonce = decoded['nonce']
+			self.sequence = decoded['sequence']+1
+			response = self.gethash()
+			message = '{"sequence":'+str(self.sequence)+', "command":"auth", "username":"'+self.username+'", "nonce":"'+decoded['nonce']+'", "response":"'+response+'", "realm":"'+decoded['realm']+'"}'
+			self.sock.sendto(message, (self.UDP_IP, self.UDP_PORT))
+			self.sequence = decoded['sequence']+1
+			data, addr = self.sock.recvfrom(1024)
+			decoded = json.loads(data)
+			if(decoded.get('error', 'success') == 'success'):
+				self.screen.clear()
+				self.screen.refresh()
+				self.screen.addstr(0,0,'authenticated')
+			else:
+				self.screen.clear()
+				self.screen.refresh()
+				self.screen.addstr(0,0,'error authenticating')
+
+	def gethash(self):
+		m = md5.new()
+  		m.update(self.username+':'+self.realm+':'+self.password)
+		ha1 = m.hexdigest()
+		m.update('AUTH:'+str(self.sequence))
+		ha2 = m.hexdigest()
+		m.update(ha1+':'+self.nonce+':'+ha2)
+		return m.hexdigest()
+
 def main(screen):
-	comm = Communication()
+
+	comm = Communication(screen, username, password, ip, port, sourceport)
 	screen.nodelay(1)
 	while True:
 	    char = screen.getch()
 	    if char == 113: break  # q
+	    elif char == 97: comm.auth() # a = authentication
 	    elif char == curses.KEY_RIGHT: comm.sendmessage('moveright')
 	    elif char == curses.KEY_LEFT: comm.sendmessage('moveleft')
 	    elif char == curses.KEY_UP: comm.sendmessage('moveforward')
@@ -30,4 +84,20 @@ def main(screen):
 	    time.sleep(0.1)
 
 if __name__ == '__main__':
-    curses.wrapper(main)
+	parser = argparse.ArgumentParser(description='An application that sends control messages to picard')
+
+	parser.add_argument('-u', '--username', nargs=1, dest='username', default='', required=True, help='the username')
+	parser.add_argument('-p', '--password', nargs=1, dest='password', default='', required=True, help='the password')
+	parser.add_argument('--ip', nargs=1, dest='ip', default='127.0.0.1', help='the ip')
+	parser.add_argument('--port', nargs=1, dest='port', default=[5005], help='the port')
+	parser.add_argument('--sourceport', nargs=1, dest='sourceport', default=[6666], help='the sourceport')
+	parser.add_argument('--version', action='version', dest='version', version='%(prog)s 0.1')
+
+	results = parser.parse_args()
+
+	username = results.username[0]
+	password = results.password[0]
+	ip = results.ip[0]
+	port = int(results.port[0])
+	sourceport = int(results.sourceport[0])	
+	curses.wrapper(main)
--- picard.py
+++ picard.py
@@ -1,20 +1,90 @@
 import socket
 import json
+import random
+from Crypto.Random.random import getrandbits
+import md5
+import ConfigParser
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
+class Auth:
+  def __init__(self):
+    self.UDP_IP = '127.0.0.1'
+    self.UDP_PORT = 5005
+    self.sequence = 0
+    self.sock = socket.socket(socket.AF_INET, # Internet
+                       socket.SOCK_DGRAM) # UDP
+    self.sock.bind((self.UDP_IP, self.UDP_PORT))
+    self.realm = 'picard@raspberrykai'
+    self.username = 'kai'
+    self.password = 'test'
+    self.Config = ConfigParser.ConfigParser()
+    self.Config.read('users.ini')
+    self.nonces = {}
+    self.sequences = {}
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((UDP_IP, UDP_PORT))
+  def lookup(self,username):
+    if self.Config.has_option('users', username):
+      return True
+    else:
+      return False
 
-while True:
-  data, addr = sock.recvfrom(1024)
-  print "received message:", data
+  def loop(self):
+    while True:
+      data, addr = self.sock.recvfrom(1024)
+      print "received message:", data, addr
+      self.lookup('kai')
+      try:
+        decoded = json.loads(data)
+        print 'DECODED:', decoded
+        print decoded['sequence']
+        if decoded['command'] == 'requestauth':
+          if(self.lookup(decoded['username'])):
+            self.nonces[decoded['username']] = str(getrandbits(128))
+            self.sequences[decoded['username']] = random.randint(1,65536)
+            print self.nonces[decoded['username']]
+            print self.sequences[decoded['username']]
+            message = '{"sequence":'+str(self.sequences[decoded['username']])+',"realm":"'+self.realm+'", "nonce":"'+self.nonces[decoded['username']]+'"}'
+          else:
+            message = '{"error":"username not found"}'
+          self.sock.sendto(message, (self.UDP_IP, addr[1]))
+        elif decoded['command'] == 'auth':
+          print 'yay'
+          m = md5.new()
+          m.update(self.username+':'+decoded['realm']+':'+self.password)
+          ha1 = m.hexdigest()
+          m.update('AUTH:'+str(decoded['sequence']))
+          ha2 = m.hexdigest()
+          m.update(ha1+':'+self.nonces[decoded['username']]+':'+ha2)
+          response = m.hexdigest()
 
-  try:
-    decoded = json.loads(data)
-    print 'DECODED:', decoded
-    print decoded['sequence']
-  except ValueError, e:
-    print 'JSON decoding failed: '
-    print e
+          if(response == decoded['response']):
+            print 'authenticated'
+            self.sequences[decoded['username']] = decoded['sequence']
+            message = '{"info":"authenticated"}'
+          else:
+            message = '{"error":"unauthorized"}'
+          self.sock.sendto(message, (self.UDP_IP, addr[1]))            
+        else:
+          print 'check hash'
+          print self.username
+          print self.realm
+          m = md5.new()
+          m.update(self.username+':'+self.realm+':'+self.password)
+          ha1 = m.hexdigest()
+          m.update('AUTH:'+str(decoded['sequence']))
+          ha2 = m.hexdigest()
+          m.update(ha1+':'+self.nonces[decoded['username']]+':'+ha2)
+          if(decoded['hash'] == m.hexdigest()):
+            print 'correct hash'
+            print self.sequences[decoded['username']]
+            if(self.sequences[decoded['username']] < decoded['sequence']):
+              # execute command
+              self.sequences[decoded['username']] = decoded['sequence']
+              print 'received sequence > stored sequence'
+
+      except ValueError, e:
+        print 'JSON decoding failed: '
+        print e
+
+if __name__ == '__main__':
+  auth = Auth()
+  auth.loop()
